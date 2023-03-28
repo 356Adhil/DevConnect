b25628b43f579b0aee7c990b07a5c8a8b7f74ed4
Adhil Akbar <356adhil@gmail.com>
2023-03-28 15:52:25 +0530

    Rich text editor added.

--- frontend/src/components/user/articles/ArticlePage.jsx
+++ frontend/src/components/user/articles/ArticlePage.jsx
@@ -9,12 +9,11 @@ import { setArticleData } from "../../../Redux/features/articleSlice";
 import profile from "../../../assets/Profile.jpg";
 import { useNavigate } from "react-router-dom";
 import articleCover from "../../../assets/articleCover.webp";
-import JoditEditor from 'jodit-react';
+import JoditEditor from "jodit-react";
 
 function ArticlePage() {
-
   const editor = useRef(null);
-	const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [refresh, setRefresh] = useState(false);
@@ -48,11 +47,11 @@ function ArticlePage() {
 
     try {
       // console.log(formImg);
-      let form = new FormData()
-      form.append('title',formData.title)
-      form.append('content', content)
+      let form = new FormData();
+      form.append("title", formData.title);
+      form.append("content", content);
       // form.append('image',formImg)
-   
+
       const response = await axios.post(
         "http://localhost:4000/articles",
         form,
@@ -90,13 +89,14 @@ function ArticlePage() {
     <>
       {isModalOpen && (
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-800 bg-opacity-40 z-50 flex items-center justify-center backdrop-blur-sm">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-1/2">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full m-4 md:m-0 md:w-1/2">
             <h2 className="text-xl font-bold mb-4">Add Article</h2>
             <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
               <label className="text-sm font-medium" htmlFor="title">
                 Title
               </label>
               <input
+                required
                 className="border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="title"
                 name="title"
@@ -107,49 +107,58 @@ function ArticlePage() {
               />
 
               {/* <label className="text-sm font-medium" htmlFor="image">
-                Cover Image
-              </label>
-              <input
-                className="border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="image"
-                name="image"
-                type="file"
-                onChange={((event)=>{
-                  event.preventDefault()
-                  handleImageChange(event)
-                })}
-                accept="image/*"
-                required
-              /> */}
+              Cover Image
+            </label>
+            <input
+              className="border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="image"
+              name="image"
+              type="file"
+              onChange={((event)=>{
+                event.preventDefault()
+                handleImageChange(event)
+              })}
+              accept="image/*"
+              required
+            /> */}
 
               <label className="text-sm font-medium" htmlFor="content">
                 Content
               </label>
-              
-              <JoditEditor 
-              ref={editor}
-              value={content}
-              onChange={newContent=>setContent(newContent)}
-              />
-      
-              <button
-                onClick={() => {
-                  setRefresh(!refresh);
-                }}
-                className="bg-primary text-white py-2 px-4 rounded-lg"
-                type="submit"
-              >
-                Save
-              </button>
-              <button
-                className="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg"
-                onClick={() => setIsModalOpen(false)}
-              >
-                Cancel
-              </button>
+
+              <div className="h-64 overflow-y-auto">
+                <JoditEditor
+                  ref={editor}
+                  value={content}
+                  onChange={(newContent) => setContent(newContent)}
+                />
+              </div>
+
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                <button
+                  onClick={() => {
+                    setRefresh(!refresh);
+                  }}
+                  className="bg-primary text-white py-2 px-4 rounded-lg w-full md:w-auto"
+                  type="submit"
+                >
+                  Save
+                </button>
+                <button
+                  className="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg w-full md:w-auto"
+                  onClick={() =>{
+                    setIsModalOpen(false);
+                    setFormData("");
+                    setContent("");
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
             </form>
           </div>
         </div>
+
       )}
 
       <div className="flex justify-between md:p-10 p-6">
@@ -167,45 +176,58 @@ function ArticlePage() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-8">
-  {articles.map((article) => (
-    <div key={article._id} className="bg-white shadow-md rounded-md overflow-hidden flex flex-col cursor-pointer" onClick={()=> handleClick(article)}>
-      <img src={articleCover} alt="Article Image" className="w-full hover:shadow-lg" />
-      <div className="px-4 py-3 flex-grow">
-        <h2 className="text-xl font-medium mb-2">{article.title}</h2>
-        <div className="h-20 overflow-hidden">
-        <p className="text-gray-700 text-base mb-4">
-        <div dangerouslySetInnerHTML={{ __html: article.content.slice(0, 140) + '...' }}></div>
+        {articles.map((article) => (
+          <div
+            key={article._id}
+            className="bg-white shadow-md rounded-md overflow-hidden flex flex-col cursor-pointer"
+            onClick={() => handleClick(article)}
+          >
+            <img
+              src={articleCover}
+              alt="Article Image"
+              className="w-full hover:shadow-lg"
+            />
+            <div className="px-4 py-3 flex-grow">
+              <h2 className="text-xl font-medium mb-2">{article.title}</h2>
+              <div className="h-20 overflow-hidden">
+                <div className="text-gray-700 text-base mb-4">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: article.content.slice(0, 140) + "...",
+                    }}
+                  ></div>
 
-          {article.content.length > 140 && (
-            <button
-              className="text-blue-600 hover:underline text-sm"
-              onClick={()=> handleClick(article)}
-            >
-              See More
-            </button>
-          )}
-        </p>
-        </div>
-      </div>
-      <div className="mt-auto px-4 py-3">
-        <div className="flex items-center">
-          <img
-            src={profile}
-            alt="Author Image"
-            className="w-10 h-10 rounded-full mr-2"
-          />
-          <div>
-            <p className="text-gray-900 font-medium">{article.userName}</p>
-            <p className="text-gray-600 text-sm">
-              Published on {article.createdDate}
-            </p>
+                  {article.content.length > 140 && (
+                    <button
+                      className="text-blue-600 hover:underline text-sm"
+                      onClick={() => handleClick(article)}
+                    >
+                      See More
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="mt-auto px-4 py-3">
+              <div className="flex items-center">
+                <img
+                  src={profile}
+                  alt="Author Image"
+                  className="w-10 h-10 rounded-full mr-2"
+                />
+                <div>
+                  <p className="text-gray-900 font-medium">
+                    {article.userName}
+                  </p>
+                  <p className="text-gray-600 text-sm">
+                    Published on {article.createdDate}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-    </div>
-  ))}
-</div>
-
     </>
   );
 }
--- frontend/src/components/user/profile/Profile.jsx
+++ frontend/src/components/user/profile/Profile.jsx
@@ -116,7 +116,7 @@ export default function Profile() {
                   </div>
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2">
+                  <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2 uppercase">
                     {userDetails?.fullName}
                   </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
